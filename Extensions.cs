2c66530b0a7ee8244a9041a3ba32e24a7624a62b
talrand <jamesknopp1988@hotmail.com>
2021-01-10 09:47:00 +0000

    Added Extensions class

---
+++ Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Talrand.Core
+{
+    public static class Extensions
+    {
+        public static byte ToNumeric(this Boolean val)
+        {
+            return Conversions.BooleanToNumeric(val);
+        }
+
+        public static bool ToBoolean(this Int32 val)
+        {
+            return Conversions.NumericToBoolean(val);
+        }
+
+        public static bool ToBoolean(this Byte val)
+        {
+            return Conversions.NumericToBoolean(val);
+        }
+    }
+}
