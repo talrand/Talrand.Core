5584bed1cd40e4be7c8191cc55233ed03c5304b1
talrand <jamesknopp1988@hotmail.com>
2021-01-17 09:26:59 +0000

    Added Join function to string together array items

--- Extensions.cs
+++ Extensions.cs
@@ -4,7 +4,7 @@ namespace Talrand.Core
 {
     public static class Extensions
     {
-        public static byte ToNumeric(this Boolean val)
+        public static byte ToByte(this Boolean val)
         {
             return Conversions.BooleanToNumeric(val);
         }
@@ -17,6 +17,51 @@ namespace Talrand.Core
         public static bool ToBoolean(this Byte val)
         {
             return Conversions.NumericToBoolean(val);
+        }
+
+        public static string Join(this String val, string delimiter, string val2, string val3 = "", string val4 = "", string val5 = "", string val6 = "")
+        {
+            return Generic.StringTogether(delimiter, val, val2, val3, val4, val5, val6);
+        }
+
+        /// <summary>
+        /// Strings together items in a string array with the specified delimiter
+        /// </summary>
+        /// <param name="val"></param>
+        /// <param name="delimiter">Delimiter to use between items in the array</param>
+        /// <returns></returns>
+        public static string Join(this String[] val, string delimiter)
+        {
+            string joinedVal = "";
+
+            // Loop through array and join strings together
+            for(int i = 0; i < val.Length; i++)
+            {
+                joinedVal = joinedVal.Join(delimiter, joinedVal, val[i]);
+            }
+
+            // Return constructed string
+            return joinedVal;
+        }
+
+        /// <summary>
+        /// Strings together items in an integer array with the specified delimiter
+        /// </summary>
+        /// <param name="val"></param>
+        /// <param name="delimiter">Delimiter to use between items in the array</param>
+        /// <returns></returns>
+        public static string Join(this int[] val, string delimiter)
+        {
+            string joinedVal = "";
+
+            // Loop through array and join strings together
+            for (int i = 0; i < val.Length; i++)
+            {
+                joinedVal = joinedVal.Join(delimiter, joinedVal, val[i].ToString());
+            }
+
+            // Return constructed string
+            return joinedVal;
         }
     }
 }
