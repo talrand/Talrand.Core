15b54159dc9d8f88701424170370cb1808d268b6
talrand <jamesknopp1988@hotmail.com>
2021-01-24 09:23:44 +0000

    Added support for CC and BCC

--- Email.cs
+++ Email.cs
@@ -6,7 +6,7 @@ namespace Talrand.Core
 {
     public class Email
     {
-        private Collection<string> Recipients = new Collection<string>();
+        private Collection<Recipient> Recipients = new Collection<Recipient>();
         public string Subject { get; set; }
         public string Body { get; set; }
         public string FromEmail { get; set; }
@@ -31,13 +31,32 @@ namespace Talrand.Core
             }
         }
 
-        /// <summary>
-        /// Add email address to recipients collection
-        /// </summary>
-        /// <param name="emailAddress">Email address to add to recipient collection</param>
-        public void AddRecipient(string emailAddress)
+        public struct Recipient
         {
-            Recipients.Add(emailAddress);
+            public string EmailAddress { get; set; }
+            public RecipientType Type { get; set; }
+
+            public Recipient(string emailAddress, RecipientType type)
+            {
+                EmailAddress = emailAddress;
+                Type = type;
+            }
+        }
+
+        public enum RecipientType
+        {
+            Standard,
+            CC,
+            BCC
+        }
+
+        /// <summary>
+        /// Adds a recipient to recipients collection
+        /// </summary>
+        /// <param name="recipient">A Recipient object containing details of the recipient to send email to</param>
+        public void AddRecipient(Recipient recipient)
+        {
+            Recipients.Add(recipient);
         }
 
         /// <summary>
@@ -45,27 +64,43 @@ namespace Talrand.Core
         /// </summary>
         public void Send()
         {
-            for (int i = 0; i < Recipients.Count; i++)
+            using (MailMessage message = new MailMessage())
             {
-                using (MailMessage message = new MailMessage())
-                {
-                    // Construct new message
-                    message.From = new MailAddress(FromEmail);
-                    message.To.Add(Recipients[i]);
-                    message.Subject = Subject;
-                    message.IsBodyHtml = true;
-                    message.Body = Body;
+                // Construct new message
+                message.From = new MailAddress(FromEmail);
+                message.Subject = Subject;
+                message.IsBodyHtml = true;
+                message.Body = Body;
 
-                    // Send message
-                    using (SmtpClient smtpClient = new SmtpClient(SMTPSettings.Server, SMTPSettings.Port))
+                // Add recipients to message based on type
+                foreach (Recipient recipient in Recipients)
+                {
+                    switch (recipient.Type)
                     {
-                        smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
-                        smtpClient.UseDefaultCredentials = false;
-                        smtpClient.Credentials = new System.Net.NetworkCredential(SMTPSettings.UserName, SMTPSettings.Password);
-                        smtpClient.EnableSsl = SMTPSettings.UseSSL;
-                        smtpClient.Send(message);
+                        case RecipientType.Standard:
+                            message.To.Add(recipient.EmailAddress);
+                            break;
+                        case RecipientType.CC:
+                            message.CC.Add(recipient.EmailAddress);
+                            break;
+                        case RecipientType.BCC:
+                            message.Bcc.Add(recipient.EmailAddress);
+                            break;
                     }
                 }
+
+                // Send message
+                using (SmtpClient smtpClient = new SmtpClient(SMTPSettings.Server, SMTPSettings.Port))
+                {
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtpClient.UseDefaultCredentials = false;
+                    smtpClient.Credentials = new System.Net.NetworkCredential(SMTPSettings.UserName, SMTPSettings.Password);
+                    smtpClient.EnableSsl = SMTPSettings.UseSSL;
+                    smtpClient.Send(message);
+                }
+
+                // Clear recipients to ensure recipients aren't included in the next email
+                Recipients.Clear();
             }
         }
     }
