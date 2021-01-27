f470c2de4cbeb3681212f615654f8a36f9c222ce
James <jamesknopp1988@hotmail.com>
2021-01-27 13:37:24 +0000

    Added class for writing log files

---
+++ LogFile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Talrand.Core
+{
+    public class LogFile
+    {
+        public string FileName { get; set; }
+
+        public LogFile(string fileName)
+        {
+            FileName = fileName;
+        }
+
+        /// <summary>
+        /// Outputs passed text to log file
+        /// </summary>
+        /// <param name="text">Text to append to log</param>
+        public void Write(string text)
+        {
+            using (StreamWriter fileWriter = File.AppendText(FileName))
+            {
+                fileWriter.WriteLine(DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss") + " : " + text);
+            }
+        }
+
+        /// <summary>
+        /// Deletes log file
+        /// </summary>
+        public void Delete()
+        {
+            if (File.Exists(FileName) == true)
+            {
+                File.Delete(FileName);
+            }
+        }
+    }
+}
