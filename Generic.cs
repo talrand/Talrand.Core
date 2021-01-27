67f4f1520707efd99e272183d50fb6b3743fe2b1
James <jamesknopp1988@hotmail.com>
2021-01-27 09:26:17 +0000

    Moved read and write registry keys to a new Registry class

--- Generic.cs
+++ Generic.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Microsoft.Win32;
 using System.Diagnostics;
 
 namespace Talrand.Core
@@ -13,67 +12,6 @@ namespace Talrand.Core
         public static string ExecutablePath()
         {
             return System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-        }
-
-        /// <summary>
-        /// Read the value for the passed registry key
-        /// </summary>
-        /// <param name="keyPath">Path to registry key</param>
-        /// <param name="keyName">Name of registry key</param>
-        /// <returns></returns>
-        public static string ReadRegistryKey(string keyPath, string keyName)
-        {
-            Object keyVal = null;
-
-            // Open registry key
-            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(keyPath))
-            {
-                if (key != null)
-                {
-                    // Get key value and close key
-                    keyVal = key.GetValue(keyName);
-                    key.Close();
-                }
-
-            }
-
-            if (keyVal != null)
-            {
-                // Convert key value to string
-                return keyVal.ToString();
-            }
-            else
-            {
-                // No value - return blank
-                return "";
-            }
-        }
-
-        /// <summary>
-        /// Writes value to the registry
-        /// </summary>
-        /// <param name="keyPath">Path to registry key</param>
-        /// <param name="keyName">Name of registry key</param>
-        /// <param name="keyVal">Value to write to registry key</param>
-        public static void WriteRegistryKey(string keyPath, string keyName, string keyVal)
-        {
-            RegistryKey key = null;
-
-            // Open registry key
-            key = Registry.CurrentUser.OpenSubKey(keyPath, true);
-
-            // Create registry key if it doesn't exist
-            if (key == null)
-            {
-                key = Registry.CurrentUser.CreateSubKey(keyPath, RegistryKeyPermissionCheck.ReadWriteSubTree);
-            }
-
-            // Set key value
-            key.SetValue(keyName, keyVal, RegistryValueKind.String);
-
-            // Close and dispose of key
-            key.Close();
-            key.Dispose();
         }
 
         /// <summary>
---
+++ Registry.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Talrand.Core
+{
+    public static class Registry
+    {
+        /// <summary>
+        /// Read the value for the passed registry key
+        /// </summary>
+        /// <param name="keyPath">Path to registry key</param>
+        /// <param name="keyName">Name of registry key</param>
+        /// <returns></returns>
+        public static string ReadKey(string keyPath, string keyName)
+        {
+            Object keyVal = null;
+
+            // Open registry key
+            using (Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(keyPath))
+            {
+                if (key != null)
+                {
+                    // Get key value and close key
+                    keyVal = key.GetValue(keyName);
+                    key.Close();
+                }
+
+            }
+
+            if (keyVal != null)
+            {
+                // Convert key value to string
+                return keyVal.ToString();
+            }
+            else
+            {
+                // No value - return blank
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Writes value to the registry
+        /// </summary>
+        /// <param name="keyPath">Path to registry key</param>
+        /// <param name="keyName">Name of registry key</param>
+        /// <param name="keyVal">Value to write to registry key</param>
+        public static void WriteKey(string keyPath, string keyName, string keyVal)
+        {
+            Microsoft.Win32.RegistryKey key;
+
+            // Open registry key
+            key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(keyPath, true);
+
+            // Create registry key if it doesn't exist
+            if (key == null)
+            {
+                key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(keyPath, Microsoft.Win32.RegistryKeyPermissionCheck.ReadWriteSubTree);
+            }
+
+            // Set key value
+            key.SetValue(keyName, keyVal, Microsoft.Win32.RegistryValueKind.String);
+
+            // Close and dispose of key
+            key.Close();
+            key.Dispose();
+        }
+    }
+}
